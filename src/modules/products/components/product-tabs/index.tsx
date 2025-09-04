4159b5d40f80abb17752357c6a6e6055d0be78a5
iamkrishsoni <iamkrishsoni@gmail.com>
2025-09-04 09:03:59 +0530

    Fix: Content of shipping & returns in product page

--- src/modules/products/components/product-tabs/index.tsx
+++ src/modules/products/components/product-tabs/index.tsx
@@ -152,7 +152,7 @@ const ShippingInfoTab = () => {
       </li>
       <li>
         We offer a 30-day return policy. If you are not completely satisfied
-        with your purchase, you can return the chair for a full refund or
+        with your purchase, you may return the item for a full refund or
         exchange, provided it is in its original condition and packaging.
       </li>
     </ul>
